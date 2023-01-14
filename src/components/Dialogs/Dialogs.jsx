aa00348886ce8c79094b4af62897642a6133593f
unknown <ef.va@mail.ru>
2023-01-14 22:50:03 +0300

    update messages and dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,33 @@ import React from 'react';
 import s from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Taya
+                </div>
+                <div className={s.dialog}>
+                    Max
+                </div>
+                <div className={s.dialog}>
+                    Serj
+                </div>
+                <div className={s.dialog}>
+                    El'nur
+                </div>
+                <div className={s.dialog}>
+                    Dima
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yoyoyo</div>
+                <div className={s.message}>Welcome to the club, buddy</div>
+                <div className={s.message}>Let's go</div>
+            </div>
         </div>
+
     );
 };
 export default Dialogs;
