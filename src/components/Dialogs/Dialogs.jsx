ac097b26ebe6b9c95f5550d538b77ddc47c15188
unknown <ef.va@mail.ru>
2023-01-17 20:16:16 +0300

    added 'props' for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
+
+const DialogItem = (props) => {
+    let path = '/messages/' + props.id;
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    );
+};
+
+const Message = (props) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    );
+};
+
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    <NavLink to='/messages/1'> Taya </NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/messages/2'> Max </NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/messages/3'> Serj </NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/messages/4'> El'nur </NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/messages/5'> Dima </NavLink>
-                </div>
+                <DialogItem name='Taya' id='1' />
+                <DialogItem name='Max' id='2' />
+                <DialogItem name='Serj' id='3' />
+                <DialogItem name='Elnur' id='4' />
+                <DialogItem name='Dima' id='5' />
+                <DialogItem name='Simba' id='6' />
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>How are you?</div>
-                <div className={s.message}>Yoyoyo</div>
-                <div className={s.message}>Welcome to the club, buddy</div>
-                <div className={s.message}>Let's go</div>
+                <Message message='Hi'/>
+                <Message message='How are you?'/>
+                <Message message='Yoyoyo'/>
+                <Message message='Welcome to the club, buddy'/>
+                <Message message='Lets go'/>
+                <Message message='Meow Meoooow'/>
             </div>
         </div>
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,15 +3,19 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 const MyPosts = () => {
   return (
-    <div>
-      My posts
+    <div className={s.postsBlock}>
+      <h3>My posts</h3>
       <div>
-        <textarea></textarea>
-        <button>Add post</button>
+        <div>
+          <textarea></textarea>
+        </div>
+        <div>
+          <button>Add post</button>
+        </div>
       </div>
       <div className={s.posts}>
         <Post message='Hi, how are you' likesCount='33M' />
-        <Post message='It is my first post' likesCount='25k'/>
+        <Post message='It is my first post' likesCount='25k' />
       </div>
     </div>
   );
