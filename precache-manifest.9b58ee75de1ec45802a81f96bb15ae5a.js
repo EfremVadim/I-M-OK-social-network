91b3a39d81b3fb3a4c9252947dc224dfe5cfe69f
EfremVadim <ef.va@mail.ru>
2023-04-22 11:02:51 +0300

    Updates

---
+++ precache-manifest.9b58ee75de1ec45802a81f96bb15ae5a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "627dc15afcab4f37f199",
+    "url": "/I-M-OK-social-network/static/css/main.62cc0c69.chunk.css"
+  },
+  {
+    "revision": "627dc15afcab4f37f199",
+    "url": "/I-M-OK-social-network/static/js/main.79a4ee1c.chunk.js"
+  },
+  {
+    "revision": "3a40e8f60488f41e4430",
+    "url": "/I-M-OK-social-network/static/js/runtime~main.299c2f60.js"
+  },
+  {
+    "revision": "a4414b8af3bdba08b47c",
+    "url": "/I-M-OK-social-network/static/js/2.5616d71f.chunk.js"
+  },
+  {
+    "revision": "809bd05de826ac21cf90",
+    "url": "/I-M-OK-social-network/static/css/3.2f8b341b.chunk.css"
+  },
+  {
+    "revision": "809bd05de826ac21cf90",
+    "url": "/I-M-OK-social-network/static/js/3.c47345ef.chunk.js"
+  },
+  {
+    "revision": "870c844326d42836100d",
+    "url": "/I-M-OK-social-network/static/css/4.f18673fe.chunk.css"
+  },
+  {
+    "revision": "870c844326d42836100d",
+    "url": "/I-M-OK-social-network/static/js/4.a8007f59.chunk.js"
+  },
+  {
+    "revision": "1bb95aac72e5a57bb8a7",
+    "url": "/I-M-OK-social-network/static/js/5.caa8ebd5.chunk.js"
+  },
+  {
+    "revision": "08109a780cbe3f4ef0aa765cf90a55cc",
+    "url": "/I-M-OK-social-network/static/media/user.08109a78.png"
+  },
+  {
+    "revision": "22119780a3279de0fa3d6f206f45e45c",
+    "url": "/I-M-OK-social-network/static/media/preloader.22119780.svg"
+  },
+  {
+    "revision": "c0661435e8b6986d77f77ae6c390c1b7",
+    "url": "/I-M-OK-social-network/static/media/headerIcon.c0661435.jpg"
+  },
+  {
+    "revision": "27379e7be6a7db239ca5d41101d8b2d5",
+    "url": "/I-M-OK-social-network/index.html"
+  }
+];
